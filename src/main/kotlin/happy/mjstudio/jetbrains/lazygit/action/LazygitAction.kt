1decff86648ba202753efc6900768272c02f007f
MJ <mym0404@gmail.com>
2025-09-02 18:20:25 +0900

    chore: unconscious commit 🎲

--- src/main/kotlin/happy/mjstudio/jetbrains/lazygit/action/LazygitAction.kt
+++ src/main/kotlin/happy/mjstudio/jetbrains/lazygit/action/LazygitAction.kt
@@ -5,21 +5,12 @@ package happy.mjstudio.jetbrains.lazygit.action
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.terminal.JBTerminalWidget
-import com.intellij.terminal.ui.TerminalWidget
-import happy.mjstudio.jetbrains.lazygit.terminal.LazyGitShellTerminalRunner
-import org.jetbrains.plugins.terminal.AbstractTerminalRunner
-import org.jetbrains.plugins.terminal.ShellTerminalWidget
-import org.jetbrains.plugins.terminal.TerminalTabState
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
-import java.util.*
 
 class LazygitAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        val workingDirectory = project.basePath ?: return
 
         ToolWindowManager.getInstance(project)
 
@@ -28,12 +19,13 @@ class LazygitAction : AnAction() {
 
         // Check if terminal tool window was initially visible
         val wasTerminalVisible = terminalToolWindow?.isVisible == true
-        println("wasTerminalVisible $wasTerminalVisible")
 
-        val terminalWidget = createFastTerminal(project, terminalManager, workingDirectory, "Lazygit")
+        val terminalWidget =
+            terminalManager.createNewSession().apply {
+                sendCommandToExecute("lazygit")
+            }
 
         // Execute lazygit command
-        terminalWidget.executeCommand("lazygit")
         terminalManager.toolWindow.show {
             // Wait a moment for the terminal to be ready, then move to editor
             terminalWidget.requestFocus()
@@ -57,47 +49,5 @@ class LazygitAction : AnAction() {
                 true,
             )
         }
-    }
-
-    private fun createFastTerminal(
-        project: Project,
-        terminalManager: TerminalToolWindowManager,
-        workingDirectory: String,
-        tabName: String,
-    ): ShellTerminalWidget {
-        val tabState = TerminalTabState()
-        tabState.myTabName = tabName
-        tabState.myWorkingDirectory = workingDirectory
-
-        val widget = openWithRunnerAndGetWidget(terminalManager, LazyGitShellTerminalRunner(project), tabState)
-        return Objects.requireNonNull(JBTerminalWidget.asJediTermWidget(widget)) as ShellTerminalWidget
-    }
-
-    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-    private fun openWithRunnerAndGetWidget(
-        terminalManager: TerminalToolWindowManager,
-        runner: AbstractTerminalRunner<*>,
-        tabState: TerminalTabState?,
-    ): TerminalWidget {
-        terminalManager.createNewSession(runner, tabState)
-
-        val tw = terminalManager.toolWindow
-        val cm = tw.contentManager
-
-        for (c in cm.contents) {
-            val r = TerminalToolWindowManager.getRunnerByContent(c)
-            if (r === runner) {
-                return Objects.requireNonNull<TerminalWidget?>(
-                    TerminalToolWindowManager.findWidgetByContent(c),
-                    "TerminalWidget not found for content",
-                )
-            }
-        }
-
-        val selected = cm.selectedContent
-        return Objects.requireNonNull<TerminalWidget?>(
-            TerminalToolWindowManager.findWidgetByContent(selected!!),
-            "Selected TerminalWidget not found",
-        )
     }
 }
